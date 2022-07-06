31d7b27a2b9850cc8134daf37629a097d2db6d67
Dmitriy_Pyanzin <lysy_41@mail.ru>
2022-07-06 16:46:50 +0300

    Added_MySignature

---
+++ Signatire/C#/Desision/Program.cs
@@ -0,0 +1,10 @@
+﻿void MySignature()
+{
+    Console.WriteLine();
+    Console.WriteLine("________________________");
+    Console.WriteLine("the program was implemented by Dmitry Pyanzin");
+    Console.WriteLine("________________________");
+    Console.WriteLine();
+}
+
+MySignature();
---
+++ Signatire/Python/MySignature.py
@@ -0,0 +1,8 @@
+def MySignature():
+    print()
+    print("________________________")
+    print("the program was implemented by Dmitry Pyanzin")
+    print("________________________")
+    print()
+
+MySignature()
